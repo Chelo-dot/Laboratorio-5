bdacc5ad1331497d5937d9b41e19844f4a4639d5
Sofi-Castro <castrosalassofia@gmail.com>
2026-06-30 22:21:30 -0600

    Corrección de errores

--- src/stack_arr.c
+++ src/stack_arr.c
@@ -3,35 +3,35 @@
 
 void sa_init(StackArr *s, int cap){
 
-    DinaArray arr = crear_arreglo(cap);
-    &s->arr;
+    DinaArray arreglo = crear_arreglo(cap);
+    s->arr = arreglo;
 
 }
 
 void sa_push(StackArr *s, int valor){
 
-    agregar_elemento(&s->arr, s->arr.size - 1, valor);
+    agregar_elemento(&s->arr, s->arr.size-s->arr.capacidad, valor);
 
 }
 
 int sa_pop(StackArr *s){
 
-    int valor_arriba = *(s->arr.data + (s->arr.size - 1));
-    eliminar_elemento(&s->arr, s->arr.size - 1);
+    int valor_arriba = *(s->arr.data + (s->arr.size-s->arr.capacidad - 1));
+    eliminar_elemento(&s->arr, s->arr.size-s->arr.capacidad - 1);
     return valor_arriba;
 
 }
 
 int sa_peek(StackArr *s){
 
-    int valor_arriba = *(s->arr.data + (s->arr.size - 1));
+    int valor_arriba = *(s->arr.data + (s->arr.size - s->arr.capacidad - 1));
     return valor_arriba;
 
 }
 
 int sa_empty(StackArr *s){
 
-    if (s->arr.capacidad = s->arr.size){
+    if ((s->arr.capacidad) == s->arr.size){
         return 1;
     } else {
         return 0;
@@ -41,6 +41,6 @@ int sa_empty(StackArr *s){
 
 void sa_free(StackArr *s){
 
-    liberar_arreglo(&s->arr);
+    liberar_arreglo(&(s->arr));
 
 }
