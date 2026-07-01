acfeda714dd1140fe57cf25d20ad8fe07c78a844
Sofi-Castro <castrosalassofia@gmail.com>
2026-06-30 21:05:31 -0600

    Corrección de errores al utilizar funciones de arreglos dinámicos e implementación de funciones restantes

--- src/stack_arr.c
+++ src/stack_arr.c
@@ -10,20 +10,37 @@ void sa_init(StackArr *s, int cap){
 
 void sa_push(StackArr *s, int valor){
 
-    agregar_elemento(s->arr.data, s->arr.size - 1, valor);
+    agregar_elemento(&s->arr, s->arr.size - 1, valor);
 
 }
 
 int sa_pop(StackArr *s){
 
     int valor_arriba = *(s->arr.data + (s->arr.size - 1));
-    eliminar_elemento(s->arr.data, s->arr.size - 1);
+    eliminar_elemento(&s->arr, s->arr.size - 1);
     return valor_arriba;
+
+}
+
+int sa_peek(StackArr *s){
+
+    int valor_arriba = *(s->arr.data + (s->arr.size - 1));
+    return valor_arriba;
+
+}
+
+int sa_empty(StackArr *s){
+
+    if (s->arr.capacidad = s->arr.size){
+        return 1;
+    } else {
+        return 0;
+    }
 
 }
 
 void sa_free(StackArr *s){
 
-    liberar_arreglo(s->arr.data);
+    liberar_arreglo(&s->arr);
 
 }
